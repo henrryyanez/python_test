ad96aea6a6068ae7422236a9f4c8046d878b82e4
hy <henrryyanez@gmail.com>
2020-04-16 20:40:48 -0300

    Falta decodificar los valores - valor parcial

---
+++ python_test/whois_script_hy.py
@@ -0,0 +1,36 @@
+mport sys
+import socket
+from datetime import datetime as dt
+import time
+
+def whois(ip):
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("whois.arin.net", 43))
+    s.send(('n ' + ip + '\r\n').encode())
+
+    response = b""
+    print(s)
+
+    # setting time limit in secondsmd
+    startTime = time.mktime(dt.now().timetuple())
+    timeLimit = 6
+    while True:
+        elapsedTime = time.mktime(dt.now().timetuple()) - startTime
+        data = s.recv(4096)
+        response += data
+        if (not data) or (elapsedTime >= timeLimit):
+            break
+    s.close()
+
+    print(response.decode())
+    
+    def main():
+    nombre = "ip.txt"
+    with open(nombre, "r") as archivo:
+        for linea in archivo:
+        #domain = sys.argv[1];
+            ip = socket.gethostbyname("linea")#;
+            whois(ip)
+            #print(ip)
+main()
